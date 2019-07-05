3ab3e9217e44b8a239fbd4ac471690045627391b
rduuke <juuanduuke@gmail.com>
2019-07-05 17:42:09 -0500

    first commit, template basic

---
+++ index.php
@@ -0,0 +1,176 @@
+<?php // defined( '_JEXEC' ) or die( 'Restricted access' );?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Saludando</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/template.css">
+</head>
+<body>
+
+    <header>
+        <div class="content">
+            <nav>
+                <div id="logo-left">
+                    <a href="#">
+                        <img src="images/saludadno-facultad-de-medicina.png" alt="">
+                    </a>
+                </div>
+                <div id="box-menu">
+                    <a href="#">
+                        <img src="images/menu-facultad-de-medicina.png" alt="">
+                    </a>
+                </div>
+                <div id="logo-right">
+                    <a href="#">
+                        <img src="images/bid-facultad-de-medicina.png" alt="">
+                    </a>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <section id="box-search">
+        <div class="content">
+            <h2 class="saludando-title-big blue text-center">¿Qué dudas tienes?</h2>
+            <div id="search" class="big">
+                <form action="" id="search">
+                    <input type="text" placeholder="Escribe la pregunta">
+                    <button>Buscar</button>
+                </form>
+            </div>
+        </div>
+    </section>
+    <section id="box-result">
+        <div class="content">
+            <h2 class="saludando-title blue">
+                Recomendados para tí
+            </h2>
+            <h3 class="saludando-sub-title black">
+                Sigue cada uno de nuestros segmentos, te ayudaremos
+            </h3>
+            <div class="box-items-recommended">
+                <div class="box-item-recommended">
+                    <img src="images/doc.jpg" alt="">
+                    <div class="box-title-recommended">
+                        <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur modi. </a>
+                    </div>
+                </div>
+                <div class="box-item-recommended">
+                    <img src="images/doc.jpg" alt="">
+                    <div class="box-title-recommended">
+                        <a href="#">aLorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur modi. </a>
+                    </div>
+                </div>
+                <div class="box-item-recommended">
+                    <img src="images/doc.jpg" alt="">
+                    <div class="box-title-recommended">
+                        <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur modi. </a>
+                    </div>
+                </div>
+                <div class="box-item-recommended">
+                    <img src="images/doc.jpg" alt="">
+                    <div class="box-title-recommended">
+                        <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur modi. </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="box-category-top">
+        <div class="content">
+
+            <h2 class="saludando-title white">Categoria 1</h2>
+            <h3 class="saludando-sub-title white">Descripción por Palabras Claves</h3>
+            <div class="box-items-categories">
+                <div class="box-item-category one">
+                    <img src="images/doc.jpg" alt="">
+                    <div class="box-title-category">
+                        <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur modi. </a>
+                    </div>
+                </div>
+                <div class="box-item-category two">
+                    <img src="images/doc.jpg" alt="">
+                    <div class="box-title-category">
+                        <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur modi. </a>
+                    </div>
+                </div>
+                <div class="box-item-category two">
+                    <img src="images/doc.jpg" alt="">
+                    <div class="box-title-category">
+                        <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur modi. </a>
+                    </div>
+                </div>
+                <div class="box-item-category one">
+                    <img src="images/doc.jpg" alt="">
+                    <div class="box-title-category">
+                        <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur modi. </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="box-category-bottom">
+        <div class="content">
+
+            <h2 class="saludando-title blue">Categoria 2</h2>
+            <h3 class="saludando-sub-title blue">Descripción por Palabras Claves</h3>
+            <div class="box-items-categories">
+                <div class="box-item-category one">
+                    <img src="images/doc.jpg" alt="">
+                    <div class="box-title-category">
+                        <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur modi. </a>
+                    </div>
+                </div>
+                <div class="box-item-category two">
+                    <img src="images/doc.jpg" alt="">
+                    <div class="box-title-category">
+                        <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur modi. </a>
+                    </div>
+                </div>
+                <div class="box-item-category two">
+                    <img src="images/doc.jpg" alt="">
+                    <div class="box-title-category">
+                        <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur modi. </a>
+                    </div>
+                </div>
+                <div class="box-item-category one">
+                    <img src="images/doc.jpg" alt="">
+                    <div class="box-title-category">
+                        <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur modi. </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="content">
+
+            <nav id="menu">
+                <ul>
+                    <li>
+                        <a href="#">Políticas de Privacidad</a>
+                    </li>
+                    <li>
+                        <a href="#">Conócenos</a>
+                    </li>
+                    <li>
+                        <a href="#">Contáctanos</a>
+                    </li>
+                </ul>
+            </nav>
+            <div id="logo-bottom-center">
+                <img src="images/saludadno-facultad-de-medicina.png" alt="">
+            </div>
+            <div id="logot-bottom-right">
+                <p>Un proyecto de</p>
+                <img width="200" src="http://pecet-colombia.org/site/images/Logo_UdeA_fac_medicina.png" alt="">
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
